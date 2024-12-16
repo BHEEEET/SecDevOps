0f9e1fac03ff141f84ba381aa4ce8b66ec9ee87b
krkbheet@gmail.com <krkbheet@gmail.com>
2024-12-16 22:32:27 +0100

    add fixes

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 print("Hello world!")
 print("Dev")
 
-API_KEY = "12345-ABCDE"
+password = "12345"  # Dit moet gedetecteerd worden.
+
 
 eval("print('Dit is onveilig!')")
+
+eval("os.system('echo Hacked!')")  # Onveilig gebruik van eval.
