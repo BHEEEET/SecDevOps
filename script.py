5d286adf426de613c55eb7123c91602505f6d4bc
krkbheet@gmail.com <krkbheet@gmail.com>
2024-12-16 22:36:17 +0100

    fixes

--- script.py
+++ script.py
@@ -7,3 +7,9 @@ password = "12345"  # Dit moet gedetecteerd worden.
 eval("print('Dit is onveilig!')")
 
 eval("os.system('echo Hacked!')")  # Onveilig gebruik van eval.
+
+user_input = "print('Kwetsbare eval')"
+eval(user_input)  # Dit moet gedetecteerd worden door CodeQL.
+ 
+api_key = "sk_test_51H3UjbvK4jLopL3yDe0g7G0asf"
+password = "SuperSecret123"
